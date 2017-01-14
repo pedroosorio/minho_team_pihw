4f7b73d631e6d31fc530ba2a49c2c4bc668e454b
Pedro Osório <pedroosorio.eeic@gmail.com>
2017-01-14 13:17:01 +0000

    Fixes in libOmni3MD. Changed MCP3k8 into c++ class

---
+++ include/mcp3k8.h
@@ -0,0 +1,91 @@
+/* This librarie interfaces SPI 4-CHannel ADC MCP3008 using wiringPi's SPI */
+#include <unistd.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#ifndef mcp3k8_h
+#define mcp3k8_h
+
+typedef unsigned int uint;
+typedef unsigned char uchar;
+
+typedef enum SPI_DEV {SPI_DEV_0=0,SPI_DEV_1} SPI_DEV;
+/* Configuration defines */
+
+#define N_CHANNELS 8 //Number of channels of MCP3008
+#define MISSING_INIT "error: init_mcp3k8() should be called before any other operation.\n"
+
+class MCP3k8{
+   public:
+   /// \brief Constructor of class
+   /// \return - -1 on error. linux file descriptor (>0) on success.
+   /// \param channel - SPI driver channel (0 or 1)
+   /// \param fclk - SPI Clock speed in Hertz 
+   /// \param mode - SPI Mode (0,1,2,3) depending on clock phase and polarity
+   /// \param bytesWord - number of bits per word
+   /// \param vref - conversion reference voltage
+   MCP3k8(SPI_DEV channel, uint fclk, uchar mode, uchar bitsWord, float vref);
+
+   /// \brief opens SPI interface
+   /// \return - <0 for error on closing.
+   int open();
+
+   /// \brief closes SPI interface
+   /// \return - <0 for error on closing.
+   int close();
+
+   // \brief function that reads a channel
+   /// \param channel - number of the channel to be read
+   float readChannel(char channel);
+
+   /// \brief function that reads all channels and
+   /// stores the values in channels
+   /// \param vec - pointer to vector where data will be stored
+   void readAll(float *vec);
+
+   private:
+   /// \brief function that reads and writes data to SPI bus
+   /// \brief data - pointer to in and out data
+   /// \brief length - size of data
+   /// \return - <0 for error on read/write.
+   int spi_rw(uchar *data, uint length);
+
+   /// \brief vector to hold values read from each channel
+   float channels[N_CHANNELS];
+   /// \brief boolean value to define wether init function has been
+   /// already called or not
+   bool init;
+   /// \brief spi file descriptor
+   int spi_fd;
+   /// \brief device name
+   char spi_dev_name[15];
+   /// \brief spi interface channel
+   uchar spi_channel;
+   /// \brief spi clock frequency
+   uint spi_fclk;
+   /// \brief spi operation mode
+   uchar spi_mode;
+   /// \brief number of bit per word
+   uchar spi_bitsWord;
+   /// \brief standard data spi exchange structure
+   struct spi_ioc_transfer spi_type_ioc;
+   /// \brief size of spi_ioc_transfer struct
+   int sz_ioc;
+   /// \brief standard data array for mcp3k8
+   uchar data_out[3];
+   /// \brief standard data array size
+   int sz_dout;
+   /// \brief defined voltage reference in mcp3008 chip
+   float voltage_reference;
+};
+
+#endif
---
+++ src/mcp3k8.cpp
@@ -0,0 +1,142 @@
+/* This librarie interfaces SPI 4-CHannel ADC MCP3008 using wiringPi's SPI */
+#include "mcp3k8.h"
+
+
+MCP3k8::MCP3k8(SPI_DEV channel, uint fclk, uchar mode, uchar bitsWord, float vref)
+{  
+   // Default values and initializations
+   memset(channels,0,sizeof(channels));  
+   init = false;
+   spi_fd = -1;
+   memcpy(spi_dev_name,"/dev/spidev0.");
+   spi_channel = 0;
+   spi_fclk = 1000000; //1MHz
+   spi_bitsWord = 8;
+   sz_ioc = sizeof(struct spi_ioc_transfer);
+   memset(spi_type_ioc,0,sz_ioc);
+   sz_dout = 3*sizeof(uchar);
+   memset(data_out,0,sz_dout);
+   voltage_reference = 3.3;
+
+   //Assignings and SPI interface initialization
+   int ret = -1;
+   if(channel==SPI_DEV_0){strcat(spi_dev_name,"0\0");}
+   else if(channel==SPI_DEV_1){strcat(spi_dev_name,"1\0");}
+   else {perror("Invalid SPI Channel"); return ret;} 
+   printf("_MCP3008: Opening MCP3008 in %s\n",spi_dev_name);
+   
+   spi_channel = channel;
+   spi_fclk = fclk;
+   spi_mode = mode;
+   spi_bitsWord = bitsWord;
+   voltage_reference = vref;
+   
+   if(open()>=0) printf("_MCP3008: Initialization Successful.\n");
+   else printf("_MCP3008: Initialization Failed.\n");
+}
+
+int MCP3k8::open()
+{
+   spi_fd = open(spi_dev_name,O_RDWR);
+   if(spi_fd<0) { perror("_MCP3008: Couldn't open SPI Interface."); return -1; }
+   
+   ret = ioctl(spi_fd,SPI_IOC_WR_MODE,&spi_mode);
+   if(ret<0) { perror("_MCP3008: Couldn't set SPI mode (WR)."); return -1; }
+
+   ret = ioctl(spi_fd,SPI_IOC_RD_MODE,&spi_mode);
+   if(ret<0) { perror("_MCP3008: Couldn't set SPI mode (RD)."); return -1; }
+
+   ret = ioctl(spi_fd,SPI_IOC_WR_BITS_PER_WORD,&spi_bitsWord);
+   if(ret<0) { perror("_MCP3008: Couldn't set SPI Bits per Word (WR)."); return -1; }
+   
+   ret = ioctl(spi_fd,SPI_IOC_RD_BITS_PER_WORD,&spi_bitsWord);
+   if(ret<0) { perror("_MCP3008: Couldn't set SPI Bits per Word (RD)."); return -1; }
+   
+   ret = ioctl(spi_fd,SPI_IOC_WR_MAX_SPEED_HZ,&spi_fclk);
+   if(ret<0) { perror("_MCP3008: Couldn't set SPI Clock Frequency (WR)."); return -1; }
+
+   ret = ioctl(spi_fd,SPI_IOC_RD_MAX_SPEED_HZ,&spi_fclk);
+   if(ret<0) { perror("_MCP3008: Couldn't set SPI Clock Frequency (RD)."); return -1; }
+   
+   init = true;
+   
+   spi_type_ioc.tx_buf = 0;
+   spi_type_ioc.rx_buf = 0;
+   spi_type_ioc.len = 0;
+   spi_type_ioc.delay_usecs = 0 ;
+   spi_type_ioc.speed_hz = spi_fclk;
+   spi_type_ioc.bits_per_word = spi_bitsWord;
+   spi_type_ioc.cs_change = 0;
+
+   data_out[0] = 1;
+   data_out[1] = 0b1000; //default to channel 0
+   data_out[2] = 0;
+      
+   return spi_fd;
+}
+
+int MCP3k8::close()
+{
+   int ret = -1;
+   if(spi_fd<0) {
+      printf("_MCP3008: Invalid SPI Interface file descriptor.\n");
+      return ret;
+   }
+
+   ret = close(fd);
+   if(ret<0) { perror("_MCP3008: Couldn't close SPI Interface."); }
+   else { 
+      init = false; 
+      printf("_MCP3008: %s closed.\n",spi_dev_name); 
+   } 
+
+   return ret; 
+}
+
+float MCP3k8::readChannel(char channel)
+{
+   if(!init) { perror(MISSING_INIT); return -1.0; }  
+   if(channel<0 || channel>=N_CHANNELS) return -1.0;
+   else {
+      uchar data[3];
+      memcpy(data,data_out,sz_dout);
+      data[1] = 0b10000000|(((channel&7)<<4));
+      if(spi_rw(data,sz_dout)>=0){
+         int adc_val = 0;
+         adc_val = (data[1]<<8)&0b1100000000;
+         adc_val |= (data[2]&0xff);
+         return ((float)adc_val*voltage_reference)/1023.0;
+      } else perror("_MCP3008: Error reading channel.");
+      
+      return -1.0;     
+   }
+}
+
+void MCP3k8::readAll(float *vec)
+{
+   if(!init) { perror(MISSING_INIT); return; }
+   for(uint i=0;i<N_CHANNELS;i++){
+      channels[i] = read_channel_mcp3k8(vref, i);
+   }
+   memcpy(channels,vec,sizeof(channels));
+}
+
+
+int MCP3k8::spi_rw(uchar *data, uint length){
+
+   struct spi_ioc_transfer spi_transfer[length];
+   int ret = -1;
+
+   for (uint i = 0 ; i < length ; i++){
+      memcpy(&spi_transfer[i],&spi_type_ioc,sz_ioc);
+      spi_transfer[i].tx_buf = (unsigned long)(data + i);
+      spi_transfer[i].rx_buf = (unsigned long)(data + i);
+      spi_transfer[i].len = sizeof(*(data + i)) ;
+   }
+
+   ret = ioctl (spi_fd, SPI_IOC_MESSAGE(length), &spi_transfer) ;
+
+   if(ret<0){perror("_MCP3008: Couldn't read form device.\n"); }
+
+   return ret;
+}
